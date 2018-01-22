3911e8c8e2f8c4021e6cde170964235e6cc89b83
pinker <xd03122049@126.com>
2018-01-22 23:43:50 +0800

    Palindrome pro

---
+++ src/main/java/package2/LongestPalindrome.java
@@ -0,0 +1,33 @@
+package package2;
+
+
+/**
+ * @author pinker on 2018/1/22
+ * 5. Longest Palindromic Substring
+ */
+public class LongestPalindrome {
+  private int low, maxLen;
+
+  public String longestPalindrome(String s) {
+    int len = s.length();
+    if (len == 1) {
+      return s;
+    }
+    for (int i = 0; i < len; i++) {
+      extendPalindrome(s, i, i);
+      extendPalindrome(s, i, i + 1);
+    }
+    return s.substring(low, low + maxLen);
+  }
+
+  private void extendPalindrome(String s, int i, int j) {
+    while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+      i--;
+      j++;
+    }
+    if (maxLen < j - i - 1) {
+      low = i + 1;
+      maxLen = j - i - 1;
+    }
+  }
+}
---
+++ src/main/java/package2/ShortestPalindrome.java
@@ -0,0 +1,13 @@
+package package2;
+
+/**
+ * @author pinker on 2018/1/22
+ * 214. Shortest Palindrome
+ */
+public class ShortestPalindrome {
+
+  public String shortestPalindrome(String s) {
+    return "ok";
+  }
+
+}
